bc8f2959acd9b596abd0251afbe74797c73ef94a
renanziin <renan.menegheti@hotmail.com>
2018-10-31 16:16:28 -0300

    cadastraToDo

--- app/src/main/java/com/example/a587648/todolist/CadastraToDoActivity.kt
+++ app/src/main/java/com/example/a587648/todolist/CadastraToDoActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_cadastro.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 /**
  * Created by 587648 on 24/10/18.
@@ -16,21 +18,29 @@ class CadastraToDoActivity : AppCompatActivity() {
         public const val TODO: String = "ToDo" //para putExtra entre activities
     }
 
+    var todo: Todo? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
 
         //verifica se foi passado um todo para alteração
-        val todo: String? = intent.getSerializableExtra(TODO) as String?
+//        val todo: String? = intent.getSerializableExtra(TODO) as String?
+
+        todo = intent.getSerializableExtra(TODO) as Todo?
         if(todo != null){
-            carregaDados(todo)
+            carregaDados()
         }
 
         btnSalvar.setOnClickListener(){
             salvaToDo()
 
         }
-
+        if (todo == null) {
+            todo = Todo(edtTodo.text.toString())
+        }else{
+            todo?.texto = edtTodo.text.toString()
+        }
     }
 
     
@@ -45,18 +55,29 @@ class CadastraToDoActivity : AppCompatActivity() {
             return
         }
 
-        val todo = edtTodo.text.toString()
 
-        val abreLista = Intent(this, MainActivity::class.java)
-        abreLista.putExtra(TODO, todo)
-        setResult(Activity.RESULT_OK, abreLista)
-        finish()
+
+//        val abreLista = Intent(this, MainActivity::class.java)
+//        abreLista.putExtra(TODO, todo)
+//        setResult(Activity.RESULT_OK, abreLista)
+//        finish()
+        val todoDao: todoDao = AppDatabase.getInstance(this).todoDao()
+        doAsync {
+            todoDao.insert(todo!!)
+            uiThread {
+                finish()
+            }
+        }
 
     }
 
     //exibe as informações do todo na Activity
-    private fun carregaDados(todo: String) {
-        edtTodo.setText(todo)
+//    private fun carregaDados(todo: String) {
+//        edtTodo.setText(todo)
+//    }
+
+    private fun carregaDados(){
+        edtTodo.setText(todo?.texto)
     }
 
 }
